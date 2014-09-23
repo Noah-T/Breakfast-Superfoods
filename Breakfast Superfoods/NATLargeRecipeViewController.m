0aaaf67f8c3c5e7e44017b97463cf33945a5fe26
Noah-T <Nteshu@gmail.com>
2014-09-23 14:13:25 -0400

    Wired Up Sharing Buttons
    Facebook, Twitter, Email sharing buttons are now fully functional

--- Breakfast Superfoods/NATLargeRecipeViewController.m
+++ Breakfast Superfoods/NATLargeRecipeViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "NATLargeRecipeViewController.h"
+#import <Social/Social.h>
 
 @interface NATLargeRecipeViewController ()
 
@@ -29,7 +30,11 @@
     // Do any additional setup after loading the view.
     self.recipeLabel.text = self.recipeLabeltext;
     self.recipeImage.image = self.recipeImageImage;
+    
+    
+    
 }
+
 
 - (void)didReceiveMemoryWarning
 {
@@ -47,5 +52,62 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (IBAction)postToTwitter:(id)sender {
+    NSLog(@"twitter button pressed");
+    if([SLComposeViewController isAvailableForServiceType:SLServiceTypeTwitter]){
+        SLComposeViewController *twitterViewController =[SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
+        [twitterViewController setInitialText:self.recipeLabeltext];
+        [self presentViewController:twitterViewController animated:YES completion:nil];
+    }
+}
+
+- (IBAction)postToFacebook:(id)sender {
+    
+    if([SLComposeViewController isAvailableForServiceType:SLServiceTypeFacebook]){
+        SLComposeViewController *facebookViewController = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook];
+        [facebookViewController setInitialText:self.recipeLabeltext];
+        [self presentViewController:facebookViewController animated:YES completion:nil];
+    }
+}
+
+- (IBAction)sendEmail:(id)sender {
+    NSString *emailTitle = @"Test email";
+    NSString *messageBody = @"Message body";
+    NSArray *toRecepients = @[@"nteshu@gmail.com"];
+    MFMailComposeViewController *mailViewController = [[MFMailComposeViewController alloc]init];
+    mailViewController.mailComposeDelegate = self;
+    [mailViewController setSubject:@"Yummy Recipe!"];
+    [mailViewController setMessageBody:@"Here's a yummy recipe!" isHTML:YES];
+    [mailViewController setToRecipients:toRecepients];
+    
+    [self presentViewController:mailViewController animated:YES completion:nil];
+}
+
+- (void) mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error
+{
+    switch (result)
+    {
+        case MFMailComposeResultCancelled:
+            NSLog(@"Mail cancelled");
+            break;
+        case MFMailComposeResultSaved:
+            NSLog(@"Mail saved");
+            break;
+        case MFMailComposeResultSent:
+            NSLog(@"Mail sent");
+            
+            //need to add a confirmation message here
+            break;
+        case MFMailComposeResultFailed:
+            NSLog(@"Mail sent failure: %@", [error localizedDescription]);
+            break;
+        default:
+            break;
+    }
+    
+    // Close the Mail Interface
+    [self dismissViewControllerAnimated:YES completion:NULL];
+}
 
 @end
